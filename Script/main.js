e694bdd9d51b39337d1cac1f306163202acccaeb
Your NameProgrammingm <niamulhossen223@gmail.com>
2025-09-20 21:42:29 +0600

    first commit

---
+++ Script/main.js
@@ -0,0 +1,51 @@
+// console.log("niamul i will fuck you kjore ");
+// console.log("i am very documenstation problem who are you mean thak ")
+// console.log("Allah  I love you");
+// console.log("I cannot live without you");
+
+// console.log(name);
+// var name= "niamul";
+
+// const countryName = "Bangladesh";
+// console.log(countryName); 
+
+// const friends = ["rafi", "Shila", "babu"];
+// friends.push("sex");
+// console.log(friends);
+// const person = {
+//     name: ["Niamil",
+//      "koliza",
+//      "sabia",
+//      "srabon",
+//    "rafi"]
+
+// }
+
+// person.name = 'kajhdfiakf';
+// console.log(person);
+
+//  const friends = ["rafi", "Shila", "babu"];
+
+//  for (let i=0; i<friends.length; i++){
+//     const element = friends[i];
+//     console.log(element);
+//  }
+
+// Default Paratmeter
+
+// function sum(num1=0, num2=0){
+//     const result= num1+num2;
+//     console.log(result);
+// }
+
+// sum(5,50);
+
+// function sum(num1=5, num2=5){
+//     const result=num1*num2;
+//     console.log(result);
+// }
+
+// sum(15);
+
+
+
