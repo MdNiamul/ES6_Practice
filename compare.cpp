121bdedea96a48eafcc4b16e1819df556a799981
Md Niamul Islam <108489887+MdNiamul@users.noreply.github.com>
2025-12-09 22:54:36 +0600

    Add files via upload

---
+++ compare.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int num1, num2;
+
+    cout<<"Ente the number num1 :: ";
+    cin>>num1;
+
+    cout<<"Ente the number num2 :: ";
+    cin>>num2;
+
+if(num1>num2)
+{
+    cout<<"the number1 is big";
+}
+else{
+    cout<<"NUmber are small"<<num2;
+}
+
+
+    getch();
+
+
+}
