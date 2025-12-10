a4bccc4d426a542c311d582544a04ebf82bfdb3d
Md Niamul Islam <108489887+MdNiamul@users.noreply.github.com>
2025-12-10 23:44:07 +0600

    Add files via upload

---
+++ sum_mul_div_rem.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  
+    float num1, num2;
+    cout<<"Enter the number ";
+    cin>>num1;
+    cin>>num2;
+
+    int sum = num1 + num2;
+    cout<<"Sum of this number "<<sum;
+    cout<<endl;
+
+    int mul = num1 * num2;
+    cout<<"Multiplication of this number "<<mul;
+    cout<<endl;
+
+    float div = num1 / num2;
+    cout<<"Division of this number "<<div;
+    cout<<endl;
+
+    cout<<endl;
+
+    int rem = num1 / num2;
+    cout<<"Remainder of this number "<<rem;
+    cout<<endl;
+
+    cout<<endl;
+
+    
+return 0;
+
+
+
+}
